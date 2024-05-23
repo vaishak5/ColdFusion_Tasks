10b86b7fbfd7dcec293c94693e99187955cc3f08
Vaishak T S <vaishakh.ts@techversantinfotech.com>
2024-05-23 18:08:39 +0530

    Task-1

--- Question-14/Script/script.js
+++ Question-14/Script/script.js
@@ -2,8 +2,8 @@ function myFunction() {
     var fileInput = document.getElementById('myfile');
 	if (fileInput.myfile.length > 0) {
         const fileSize = fileInput.myfile.item(0).size;
-        const fileMb = fileSize / 1024 ** 2;
-        if (fileMb >= 2) {
+        const fileMb = fileSize / 1048576;
+        if (fileMb >= 1) {
 		alert("Upload image greater than 1 MB");
 	}
 }
